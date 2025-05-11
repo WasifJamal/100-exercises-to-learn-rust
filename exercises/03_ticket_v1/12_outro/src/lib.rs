869cd0c041b59f5ecf8ad8d1fcbb11998c188137
WasifJamal <wsfjamal@gmail.com>
2025-05-12 03:47:09 +0530

    outro

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,63 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+#[allow(unused)]
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(
+        product_name: String,
+        quantity: u32,
+        unit_price: u32,
+    ) -> Self {
+        let order = Self {
+            product_name,
+            quantity,
+            unit_price
+        };
+        order.validate();
+        order
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.validate();
+        self.product_name = product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.validate();
+        self.quantity = quantity
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.validate();
+        self.unit_price = unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        *self.quantity() * *self.unit_price()
+    }
+
+    fn validate(&self) {
+        if self.product_name.is_empty() || self.product_name.len() > 300 || self.quantity <= 0 || self.unit_price <= 0 {
+            panic!("Incorrect input")
+        }
+    }
+}
