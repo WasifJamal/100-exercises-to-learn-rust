83c46c1c7a91333f92915dea0262efe82a60a5ce
WasifJamal <wsfjamal@gmail.com>
2025-05-14 01:04:48 +0530

    sized

--- exercises/04_traits/08_sized/src/lib.rs
+++ exercises/04_traits/08_sized/src/lib.rs
@@ -1,7 +1,18 @@
-pub fn example() {
+pub fn example() -> usize {
     // Trying to get the size of a str (or any other DST)
     // via `std::mem::size_of` will result in a compile-time error.
     //
     // TODO: Comment out the following line and move on to the next exercise.
-    std::mem::size_of::<str>();
+    // I have changed the str to &str as &str has the size. It stores a pointer and length of the str
+    std::mem::size_of::<&str>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::example;
+
+    #[test]
+    fn should_return_size() {
+        assert_eq!(size_of::<usize>() * 2, example());
+    }
 }
