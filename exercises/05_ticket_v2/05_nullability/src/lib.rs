4efaf25ebc1a4616bb81456ce596ee51804924bc
WasifJamal <wsfjamal@gmail.com>
2025-05-16 01:18:44 +0530

    nullability

--- exercises/05_ticket_v2/05_nullability/src/lib.rs
+++ exercises/05_ticket_v2/05_nullability/src/lib.rs
@@ -1,5 +1,6 @@
 // TODO: Implement `Ticket::assigned_to` using `Option` as the return type.
 
+#[allow(unused)]
 #[derive(Debug, PartialEq)]
 struct Ticket {
     title: String,
@@ -7,6 +8,7 @@ struct Ticket {
     status: Status,
 }
 
+#[allow(unused)]
 #[derive(Debug, PartialEq)]
 enum Status {
     ToDo,
@@ -14,6 +16,7 @@ enum Status {
     Done,
 }
 
+#[allow(unused)]
 impl Ticket {
     pub fn new(title: String, description: String, status: Status) -> Ticket {
         if title.is_empty() {
@@ -36,7 +39,10 @@ impl Ticket {
         }
     }
     pub fn assigned_to(&self) -> Option<&String> {
-        todo!()
+        let Status::InProgress { assigned_to } = &self.status else {
+            return None;
+        };
+        Some(assigned_to)
     }
 }
 
